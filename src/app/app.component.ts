1b742cb8d22e79a4c8ff5f7ba6166ad63b97029f
Tymofii Pastukhov <tymofii.pastukhov@leanix.net>
2023-07-06 13:45:03 +0200

    Small style changes
    Added music, improved the styles

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,31 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, ElementRef, OnInit, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'ngrx-educational-project';
+  isMusicPlaying = false;
+  navCollapsed = true;
+
+  @ViewChild('bgMusic') bgMusic!: ElementRef;
+  
+  ngOnInit() {
+    if (!localStorage.getItem('visitedBefore')) {
+      alert('You can control the music using buttons in NavBar');
+      localStorage.setItem('visitedBefore', 'true')
+    }
+  }
+
+  playMusic() {
+    this.bgMusic.nativeElement.play();
+    this.isMusicPlaying = true;
+  }
+  
+  pauseMusic() {
+    this.bgMusic.nativeElement.pause();
+    this.isMusicPlaying = false;
+  }
 }
